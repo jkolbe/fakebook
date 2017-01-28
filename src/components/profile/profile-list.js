601a345ad9ceed5e6607c8d09a2a5dc30cb452f7
Joanna Kolbe <joannakolbe@gmail.com>
2017-01-28 11:06:44 -0500

    profile container and api request

---
+++ src/components/profile/profile-list.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ProfileList = () => (
+	<div>
+		<h1>ProfileList</h1>
+	</div>
+);
+export default ProfileList;
--- src/containers/profile/profile.js
+++ src/containers/profile/profile.js
@@ -1,9 +1,41 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
+
+import ProfileList from '../../components/profile/profile-list';
 
 export default class Profile extends Component {
+	
+	URL_BASE = 'http://uinames.com/api/';
+
+	constructor() {
+		super();
+		
+		this.state = {
+			profiles : [],
+			maxPages : 4
+		};
+	}
+
+	getProfiles() {
+		$.get(`${this.URL_BASE}?ext&amount=10&region=canada`).then(
+			(res) => { this.setState({
+				profiles : [...this.state.profiles, ...res]
+			})}
+		);
+	}
+
+	componentDidMount() {
+		this.getProfiles();
+	}
+
 	render(){
 		return (
-			<div>PROFILE</div>
+			<div className="container">
+				PROFILE
+				<ProfileList />
+				{console.log(this.state.profiles)}
+			</div>
+
 		);
 	}
 }
