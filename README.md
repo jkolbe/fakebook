d0671d14a1d291f144c7cb9ac0471d0548690697
Joanna Kolbe <joanna.kolbe@mrmiomp1523.rci.rogers.ca>
2017-12-12 13:29:21 -0500

    change default route + readme

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Fakebook
 
-Using
-https://github.com/thm/uinames
+Uses UINAMES API to get a list of random profiles. Search for users and mark them as favorites.
+
+## Install & Run
+
+```
+> npm install
+> npm start
+```
+
+### 3rd party:
+* https://github.com/thm/uinames
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 
 import Nav from '../containers/navigation/navigation';
-import Home from '../containers/home/home';
 import Profile from '../containers/profile/profile';
 
 const Routes = () => (
 	<Router history={browserHistory}>
 		<Route path="" component={Nav}>
-			<Route path="/" component={Home}/>
-			<Route path="/profile" component={Profile} />
+			<Route path="/" component={Profile}/>
 		</Route>
 	</Router>
 );
